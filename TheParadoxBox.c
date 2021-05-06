073252acdd62384e3436ec9113cfeec948f97e4c
TheParadoxBox <77028175+TheParadoxBox@users.noreply.github.com>
2021-05-06 17:37:52 -0400

    Add the actual source code
    lol

---
+++ TheParadoxBox.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Wow, you're actually looking at the source code. You're either actually somebody who programs or a dingus who thought there was something wrong with the program. */
+
+int main() {
+   printf("No files have been detected in the local directory.\n");
+   printf("If you would actually like some help finding bugs, consult TheParadoxBox (The person, not the software) on Github.\n");
+   
+   return 0;
+}
